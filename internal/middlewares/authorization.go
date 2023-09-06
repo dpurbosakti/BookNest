a951e2a2ccd6ed197d97f0a5197e11505754906d
Dwi Atmoko <dwiatmokop@gmail.com>
2023-09-06 11:56:21 +0700

    fix (auth) : fix auth logger and response

--- internal/middlewares/authorization.go
+++ internal/middlewares/authorization.go
@@ -5,14 +5,17 @@ import (
 
 	"github.com/gin-gonic/gin"
 	jwt5 "github.com/golang-jwt/jwt/v5"
+	"github.com/sirupsen/logrus"
 )
 
 func AdminAuthorization() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		userData := ctx.MustGet("userData").(jwt5.MapClaims)
 		role := userData["role"].(string)
-
+		logger := logrus.WithFields(logrus.Fields{"func": "admin_authorization", "data": userData})
+		logger.WithField("role", role).Info()
 		if role != "admin" {
+			logger.Warn("Unauthorized")
 			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
 				"error":   "Unauthorized",
 				"message": "You are not allowed to access this data",

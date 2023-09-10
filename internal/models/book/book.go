3a35e3c92da8c2b2ed2febffbfa51335165cddd7
dpurbosakti <dwiatmokop@gmail.com>
2023-09-10 09:26:14 +0700

    feature(rents) : model wip

--- internal/models/book/book.go
+++ internal/models/book/book.go
@@ -7,13 +7,13 @@ import (
 )
 
 type Book struct {
-	Id          uint    `json:"id" gorm:"primaryKey,autoIncrement"`
-	Title       string  `json:"title" gorm:"type:varchar(250)"`
-	Author      string  `json:"author" gorm:"type:varchar(250)"`
-	RentFee     float64 `json:"rent_fee"`
-	IsAvailable bool    `json:"is_available"`
-	AvailableAt *time.Time
-	CreatedAt   time.Time
-	UpdatedAt   time.Time
-	DeletedAt   gorm.DeletedAt
+	Id            uint    `json:"id" gorm:"primaryKey,autoIncrement"`
+	Title         string  `json:"title" gorm:"type:varchar(250)"`
+	Author        string  `json:"author" gorm:"type:varchar(250)"`
+	RentFeePerDay float64 `json:"rent_fee_per_day"`
+	IsAvailable   bool    `json:"is_available"`
+	AvailableAt   *time.Time
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
+	DeletedAt     gorm.DeletedAt
 }

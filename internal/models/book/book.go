bf2fab5cf163d803a8ec20ad5d0dc05afd242251
dpurbosakti <dwiatmokop@gmail.com>
2023-09-11 08:12:48 +0700

    fix(books) : fix copier

--- internal/models/book/book.go
+++ internal/models/book/book.go
@@ -7,13 +7,27 @@ import (
 )
 
 type Book struct {
-	Id            uint    `json:"id" gorm:"primaryKey,autoIncrement"`
-	Title         string  `json:"title" gorm:"type:varchar(250)"`
-	Author        string  `json:"author" gorm:"type:varchar(250)"`
-	RentFeePerDay float64 `json:"rent_fee_per_day"`
-	IsAvailable   bool    `json:"is_available"`
-	AvailableAt   *time.Time
-	CreatedAt     time.Time
-	UpdatedAt     time.Time
-	DeletedAt     gorm.DeletedAt
+	Id            uint           `json:"id" gorm:"primaryKey,autoIncrement"`
+	Title         string         `json:"title" gorm:"type:varchar(250)"`
+	Author        string         `json:"author" gorm:"type:varchar(250)"`
+	RentFeePerDay float64        `json:"rent_fee_per_day"`
+	IsAvailable   bool           `json:"is_available"`
+	AvailableAt   *time.Time     `json:"available_at" gorm:"default:true"`
+	CreatedAt     time.Time      `json:"created_at"`
+	UpdatedAt     time.Time      `json:"updated_at"`
+	DeletedAt     gorm.DeletedAt `json:"deleted_at" gorm:"index"`
+}
+
+func (b *Book) Copier(input *BookUpdateRequest) {
+	if input.Title != nil {
+		b.Title = *input.Title
+	}
+
+	if input.Author != nil {
+		b.Author = *input.Author
+	}
+
+	if input.RentFeePerDay != nil {
+		b.RentFeePerDay = *input.RentFeePerDay
+	}
 }
